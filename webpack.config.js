5d40982b48a492a6aeeb7d07fd37c8d1b4b131b9
Konstantin Terekhin <konstantin.teryohin@rubius.com>
2020-04-29 22:39:14 +0700

    configure copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -54,6 +55,10 @@ module.exports = {
       template: PATHS.src + '/index.html',
       filename: 'index.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin([
+      { from: PATHS.src + '/images', to: 'images' },
+      { from: PATHS.src + '/fonts', to: 'fonts' }
+    ])
   ]
 };
