70a79d0555c484b30c0256470cd905f5ac5cf08f
Konstantin Terekhin <konstantin.teryohin@rubius.com>
2020-05-13 18:25:59 +0700

    example of partials template

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -37,9 +36,11 @@ module.exports = {
       },
       {
         test: /\.(woff|ttf|otf|eot|woff2)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name].[ext]'
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
         }
       }
     ]
@@ -48,6 +49,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: PATHS.src + '/index.html',
       filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: PATHS.src + '/about.html',
+      filename: 'about.html'
     }),
     new MiniCssExtractPlugin(),
     new CopyWebpackPlugin([
